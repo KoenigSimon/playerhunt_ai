825be211dfa6afb5bc99c30646454e26c2dd5634
Simon König <koenig.simon@t-online.de>
2023-10-31 20:10:08 +0100

    do math

--- math_helper.py
+++ math_helper.py
@@ -1,7 +1,12 @@
 import data_structs as data
+import math
 
 def distance(a: data.Vector3, b: data.Vector3):
-    pass
+    return math.sqrt(math.pow(a.x-b.x,2)+math.pow(a.y-b.y,2)+math.pow(a.z-a.z,2))
+def distance(a: data.Vector2, b: data.Vector2):
+    return math.sqrt(math.pow(a.x-b.x,2)+math.pow(a.y-b.y,2))
 
 def magnitude(vector: data.Vector3):
-    pass
+    return math.sqrt(math.pow(vector.x, 2)+math.pow(vector.y, 2)+math.pow(vector.z, 2))
+def magnitude(vector: data.Vector2):
+    return math.sqrt(math.pow(vector.x, 2)+math.pow(vector.y, 2))
