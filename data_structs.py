c99262735209f7910147085aef784c41ff4ea34b
Simon König <koenig.simon@t-online.de>
2023-10-31 21:46:18 +0100

    implemented dynamic parsing into data structs

--- data_structs.py
+++ data_structs.py
@@ -1,19 +1,39 @@
-from dataclasses import dataclass
-
-@dataclass
 class Vector3:
     x: float = 0.0
     y: float = 0.0
     z: float = 0.0
+    def parse(self, s:str):
+        self.x, self.y, self.z = map(float, s.split(","))
+    def __init__(self) -> None:
+        self.x = self.y = self.z = 0.0
+    def __str__(self) -> str:
+        return str(self.x) + " " + str(self.y) + " " + str(self.z)
 
 class Vector2:
     x: float = 0.0
     y: float = 0.0
+    def parse(self, s:str):
+        self.x, self.y = map(float, s.split(","))
+    def __init__(self) -> None:
+        self.x = self.y = 0.0
+    def __str__(self) -> str:
+        return str(self.x) + " " + str(self.y)
 
-@dataclass
 class GameData:
+    # base data
     player_pos: Vector3
     self_pos: Vector3
     play_area: Vector2
-    #player heading
-    #self heading
+    player_heading: Vector3
+    self_heading: Vector3
+
+    # action data 
+    noise_position: Vector3
+
+    def __init__(self) -> None:
+        self.player_pos = Vector3()
+        self.self_pos = Vector3()
+        self.play_area = Vector2()
+        self.player_heading = Vector3()
+        self.self_heading = Vector3()
+        self.noise_position = Vector3()
--- game_logic.py
+++ game_logic.py
@@ -4,10 +4,30 @@ def init_game():
     global game_data
     game_data = data.GameData()
 
-def parse_input_data(message):
-    pass
+def props(cls):   
+  return [i for i in cls.__dict__.keys() if i[:1] != '_']
+
+#message supports only one field per message, keep that in mind
+def parse_input_data(message: str):
+    for name in props(game_data):
+        if message.startswith(name):
+            raw_data = message.removeprefix(name).strip()
+            game_data.__getattribute__(name).parse(raw_data)
+            pass
 
 def validate_action(action):
     pass
 
-#move to triggers pathfinding
+#test
+if __name__ == '__main__':
+    init_game()
+    message = "player_pos 1.2,3.4,5.6"
+    parse_input_data(message)
+    message = "play_area 3.4,5.6"
+    parse_input_data(message)
+    message = "player_heading 1.2,3.4,5.6"
+    parse_input_data(message)
+    #game_data.player_pos.parse(message)
+    print( game_data.player_pos)
+    print( game_data.play_area)
+    print( game_data.player_heading)
