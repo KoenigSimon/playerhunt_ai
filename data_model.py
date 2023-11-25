c184e64e20dbc6074a79ea03363f31996823dc33
Simon König <koenig.simon@t-online.de>
2023-11-25 19:10:40 +0100

    update parsing functions

--- data_model.py
+++ data_model.py
@@ -3,7 +3,7 @@ class Vector3:
     y: float = 0.0
     z: float = 0.0
     def parse(self, s:str):
-        self.x, self.y, self.z = map(float, s.split(","))
+        self.x, self.y, self.z = map(float, s.strip('[]').split(";"))
     def __init__(self) -> None:
         self.x = self.y = self.z = 0.0
     def __str__(self) -> str:
@@ -16,7 +16,7 @@ class Vector2:
     x: float = 0.0
     y: float = 0.0
     def parse(self, s:str):
-        self.x, self.y = map(float, s.split(","))
+        self.x, self.y = map(float, s.strip('[]').split(";"))
     def __init__(self) -> None:
         self.x = self.y = 0.0
     def __str__(self) -> str:
@@ -25,12 +25,25 @@ class Vector2:
         if self.x == self.y == __value: return True
         else: return False
 
+class Vector3_List:
+    vectors = []
+    def parse_append(self, s:str):
+        self.vectors.append(Vector3.parse(s))
+    def __init__(self) -> None:
+        self.vectors = []
+    def __str__(self) -> str:
+        string = ""
+        for vec in self.vectors:
+            str(vec.x) + " " + str(vec.y) + " " + str(vec.z) + "\n"  
+        return string
+    pass
+
 class GameData:
     # base data
     player_pos: Vector3
     self_pos: Vector3
-    play_area: Vector2
-    cell_size: int
+    play_area: Vector2 # x,y amount columns/rows
+    cell_size: Vector2 # x,y size per grid cell
     player_heading: Vector3
     self_heading: Vector3
 
@@ -41,11 +54,10 @@ class GameData:
         self.player_pos = Vector3()
         self.self_pos = Vector3()
         self.play_area = Vector2()
-        self.cell_size = 0
+        self.cell_size = Vector2()
         self.player_heading = Vector3()
         self.self_heading = Vector3()
         self.noise_position = Vector3()
-
 
 #global variables
 game_data: GameData
